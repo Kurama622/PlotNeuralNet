28197ee0e1ab5408e49c68a7ecc65bc6fecd9108
demonlord1997 <2273893094@qq.com>
2020-07-16 22:09:34 +0800

    examples

--- my_examples/my_example_2.py
+++ my_examples/my_example_2.py
@@ -18,20 +18,22 @@ arch = [
     # to_Conv("input", 32, 40, "", offset="(0,0,0)", to="(0,0,0)", width=1, height=40, depth=32, caption="Input"),
     to_ConvRelu("conv1", 32, 12, 32, offset="(0,0,0)", to="(0,0,0)", width=10, height=12, depth=32, caption="Conv1" ),
     to_ConvRelu("conv2", 32, 4, 32, offset="(1.2,0,0)", to="(conv1-east)", width=10, height=4, depth=32 ,caption="Conv2"),
+    to_ConvRelu("conv3", 1, 4, 32, offset="(1.2,0,0)", to="(conv2-east)", width=10, height=4, depth=1.5 ,caption="Conv3"),
 
-    to_LinearRelu("linear1",'z', 4096 ,"(1.2,0,0)", "(conv2-east)", caption="Flatten", width=1.5, height=2,depth=60),
+    to_Flatten("linear1",'z', 128 ,"(1.2,0,0)", "(conv3-east)", caption="Flatten", width=1.5, height=2, depth=50),
 
 
-    to_Linear("linear2", 'z',512, "(1.2,0,0)", "(linear1-east)", width=1.5, height=3, depth=16, caption="Linear"),
-    to_SoftMax("soft", 4 ,"(1.2,0,0)", "(linear2-east)", caption="LogSoftMax", depth=4  ),
+    # to_Linear("linear2", 'z',512, "(1.2,0,0)", "(linear1-east)", width=1.5, height=3, depth=16, caption="Linear"),
+    to_SoftMax("soft", 4 ,"(1.2,0,0)", "(linear1-east)", caption="LogSoftMax", depth=4  ),
     # to_dashed_connection( "input", "conv1"),
     # to_dashed_connection( "conv1", "conv2"),
     # to_dashed_connection("conv2", "linear1"),
     # to_connection( "input", "conv1"),
     to_connection( "conv1", "conv2"),
-    to_connection("conv2", "linear1"),
-    to_connection("linear1", "linear2"),
-    to_connection( "linear2","soft"),
+    to_connection( "conv2", "conv3"),
+    to_connection("conv3", "linear1"),
+    # to_connection("linear1", "linear2"),
+    to_connection( "linear1","soft"),
     to_end()
     ]
 
